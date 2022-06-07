f17d9f859cf5cfe1815579aef92cc371d85423b5
Vladimir Nedorezov <nedvov@yandex.ru>
2022-06-07 10:46:31 +0300

    fix: отработать замечания на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,12 @@ let editButton = document.querySelector('.profile__edit-button');
 let addButton = document.querySelector('.profile__add-button');
 let profileCloseButton = document.querySelector('#profile-popup__close-button');
 let likes = document.querySelectorAll('.tiles__like');
-let formElement = document.querySelector('#profile-popup__container');
+let formElement = document.querySelector('#profile-popup__form');
 let nameInput = document.querySelector('#author-name-popup__input');
 let jobInput = document.querySelector('#author-job-popup__input');
 let name = document.querySelector('.profile__author-name');
 let job = document.querySelector('.profile__author-job');
 let profilePopup = document.querySelector('#profile-popup');
-
-nameInput.value = name.textContent;
-jobInput.value = job.textContent;
 
 function formSubmitHandler (evt) {
 	evt.preventDefault();
@@ -20,18 +17,16 @@ function formSubmitHandler (evt) {
     window.scrollTo(0, 0);
 }
 
-editButton.addEventListener('click', function() {document.querySelector('#profile-popup').classList.add('popup_opened')});
+function editProfile () {
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
+    profilePopup.classList.add('popup_opened');
+}
 
-profileCloseButton.addEventListener('click', function() {
-    profilePopup.classList.remove('popup_opened')
-    nameInput.value = name.textContent
-    jobInput.value = job.textContent
-});
+function closeProfile () {
+    profilePopup.classList.remove('popup_opened');
+}
 
-likes.forEach(element => {
-    element.addEventListener('click', function() {
-        element.classList.toggle('tiles__like_active')
-    })
-});
-
+editButton.addEventListener('click', editProfile);
+profileCloseButton.addEventListener('click', closeProfile);
 formElement.addEventListener('submit', formSubmitHandler);
